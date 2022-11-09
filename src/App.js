cd722c4001c3a1cd304539db5aabf8f29043e254
Georges Bueno <georges.bueno@hotmail.com>
2022-11-08 22:37:51 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+import React,{useState} from 'react'
 import './App.css';
+import Login from './Components/Login';
+import {StateContext} from './Context/StateContext';
 
 function App() {
+  const [route, setRoute] = useState("signin"); //creamos el estado de route
+  const [isSignedIn, setIsSignedIn]=useState(false);
+  const [user, setUser]=useState({});
+
+  //SETTING THE STATE BOX//////////
+  const [box, setBox] = useState({});
+ 
+  ////////////////INPUT ////////////  
+  const [input, setInput] = useState("");
+
+  /////////Setting Image///////////////////
+  const [imageUrl, setImageUrl] = useState("");
+ 
+  ////////Context APP//////////
+  let Context_value={
+    route:{
+      route,
+      setRoute
+    },
+    login:{
+      isSignedIn,
+      setIsSignedIn
+    },
+    usuario:{
+     user,
+     setUser
+    },
+    
+    img:{
+      input,
+      setInput,
+      box,
+      setBox,
+      setImageUrl, 
+      imageUrl
+    }
+  }
+  ////////////////RENDER PRINCIPAL PAGE///////////////////
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+      <StateContext.Provider value={Context_value}>
+       
+
+        {route === "signin"
+          ? <Login />
+          : (route === "home"
+            ? <div>
+              Home
+            </div>
+            : <h1>Registrar</h1>
+          )
+        }
+      </StateContext.Provider>
     </div>
   );
 }
---
+++ src/Components/Login.js
@@ -0,0 +1,99 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { StateContext } from "../Context/StateContext";//cambiar el context
+
+
+const Login = () => {
+
+    const { route, login, usuario } = useContext(StateContext);
+    const [email, setEmail] = useState("");
+    const [pass, setPass] = useState("");
+
+    const { setRoute } = route;
+    const { isSignedIn, setIsSignedIn } = login;
+    const { setUser } = usuario;
+
+    useEffect(() => {
+ 
+        if (isSignedIn) {
+
+            setRoute("home")
+        }
+        // eslint-disable-next-line
+    }, [isSignedIn])
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+        var raw = JSON.stringify({
+            "email": email,
+            "password": pass
+        });
+
+        var requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: raw,
+            redirect: 'follow'
+        };
+
+        await fetch("https://polar-anchorage-52776.herokuapp.com/login", requestOptions)
+            .then(response => response.json())
+            .then(result => {
+                // eslint-disable-next-line 
+                const res = result === "Not enable to login" ? alert("Credenciales incorrectas") :
+                    (
+                        setUser(result),
+                        setIsSignedIn(true)
+                    );
+            })
+    }
+
+
+    return (
+        <div className="  center flex flex-column justify-center ">
+            <main className="pa4 white-80 ">
+                <form onSubmit={handleSubmit} className="measure center">
+                    <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+                        <legend className="f1 fw6 ph0 mh0">Sign In</legend>
+                        <div className="mt3">
+                            <label className="db fw6 lh-copy f6" for="email-address">User</label>
+                            <input required
+                                className="pa2 input-reset ba bg-transparent white w-100"
+                                type="text"
+                                name="email-address"
+                                id="email-address"
+                                onChange={(e) => setEmail(e.target.value)}
+
+                            />
+                        </div>
+                        <div className="mv3 ">
+                            <label className="db fw6 lh-copy f6" for="password">Password</label>
+                            <input required
+                                className="b pa2 input-reset ba bg-transparent white w-100"
+                                type="password"
+                                name="password"
+                                id="password"
+                                onChange={(e) => setPass(e.target.value)}
+                            />
+                        </div>
+                        <label className="pa0 ma0 lh-copy f6 pointer center"><input type="checkbox" /> Remember me</label>
+                    </fieldset>
+                    <div >
+                        <input
+                            className="b ph3 pv2 input-reset ba white b--white bg-transparent grow pointer f6 dib"
+                            type="submit"
+                            onSubmit=""
+                            value="Sign in"
+                        />
+                    </div>
+                    <div className="lh-copy white mt3">
+                        <p onClick={() => setRoute("Register")} className="f6 link pointer dim white ">Register</p>
+                    </div>
+                </form>
+            </main>
+        </div>
+    );
+}
+export default Login;
